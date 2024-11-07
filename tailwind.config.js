8fe5d1954b215ab980dd632ac5f308b58c59a9a1
Mhamedaithssaine <mhamedaithssaine1@gmail.com>
2024-11-07 15:21:20 +0100

    configuration de tailwindcss et ajoute des fichiers du index.html et output.css et script.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.html",
+    "./assets/js/**/*.js",
+    "./assets/css/**/*.css"
+  ],
+  
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
